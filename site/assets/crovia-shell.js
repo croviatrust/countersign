5f8df5ded790c92f16fa8847945bd737df805e5d
Crovia Trust <info@croviatrust.com>
2026-09-20 11:18:27 +0000

    Press kit /press/ (boilerplate, one-liners, names, logos, live facts, press.json); og-tacet and og-press cards; nav and llms.txt updated

--- site/assets/crovia-shell.js
+++ site/assets/crovia-shell.js
@@ -244,9 +244,10 @@
       '<div class="sep"></div>' +
       '<a href="https://croviatrust.com/proof.html">Archive ledger seal</a>' +
       '<a href="/registry/seal/log/">Seal Transparency Log</a>' +
-      '<a href="/registry/seal/spec/">Seal spec &#8599;</a>' +
-      '<a href="https://datatracker.ietf.org/doc/draft-crovia-seal/" target="_blank" rel="noopener">IETF draft &#8599;</a>' +
-      '<a href="/registry/tacet/spec/">TACET spec</a>' +
+      '<a href="/registry/seal/spec/">Seal spec</a>' +
+      '<a href="/registry/tacet/spec/">TACET spec &amp; PNX</a>' +
+      '<a href="https://datatracker.ietf.org/doc/draft-crovia-tacet/" target="_blank" rel="noopener">IETF drafts &#8599;</a>' +
+      '<a href="/press/">Press kit</a>' +
       '<a href="https://github.com/croviatrust" target="_blank" rel="noopener">GitHub &#8599;</a>' +
       '<a href="mailto:info@croviatrust.com">Contact &#9993;</a>' +
       '</div></div>';
